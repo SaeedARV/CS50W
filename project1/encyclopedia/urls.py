c27607dd8c13a2756cf2d40d1d0299d626de640f
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-08-12 18:43:25 +0430

    complete the second specification

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.getContent, name="getContent"),
+    path("<str:title>", views.getContent, name="entryPage"),
 ]
