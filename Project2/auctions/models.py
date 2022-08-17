0385b607b32a3009b9bc14bc0a0ce3146c247f1e
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-08-17 20:38:40 +0430

    Models specification

--- Project2/auctions/models.py
+++ Project2/auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListings(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    imgURL = models.CharField()
+    category = models.CharField()
+
+class Bids(models.Model):
+    list = models.ForeignKey(AuctionListings, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+class Comments(models.Model):
+    list = models.ForeignKey(AuctionListings, on_delete=models.CASCADE)
+    text = models.CharField()
