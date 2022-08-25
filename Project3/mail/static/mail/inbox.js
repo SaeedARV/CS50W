b17c95716b715062beca9ad2819cb6df3e6e9eb9
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-08-25 22:18:53 +0430

    complete Send Mail specification

--- Project3/mail/static/mail/inbox.js
+++ Project3/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
 
 function compose_email() {
@@ -20,10 +21,29 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+
+  document.querySelector("#compose-form").addEventListener('submit', function () {
+    load_mailbox('sent');
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector("#compose-recipients").value,
+        subject: document.querySelector("#compose-subject").value,
+        body: document.querySelector("#compose-body").value
+      })
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        load_mailbox('sent');
+      })
+     
+  });
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
